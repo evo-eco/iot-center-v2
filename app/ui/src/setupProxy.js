02c6d1dcf163565c9658b4906eebdb11dcb346f4
Pavel Zavora <pavel.zavora@bonitoo.io>
2022-03-02 10:50:33 +0100

    feat(app/ui): setup proxy PORT from env

--- app/ui/src/setupProxy.js
+++ app/ui/src/setupProxy.js
@@ -1,39 +1,33 @@
 const {createProxyMiddleware} = require('http-proxy-middleware')
+const PORT = process.env.PORT || 5000
 
 module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: `http://localhost:${PORT}`,
       changeOrigin: true,
     })
   )
   app.use(
     '/influx',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: `http://localhost:${PORT}`,
       changeOrigin: true,
     })
   )
   app.use(
     '/mqtt',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: `http://localhost:${PORT}`,
       changeOrigin: true,
     })
   )
   app.use(
     '/kafka',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: `http://localhost:${PORT}`,
       changeOrigin: true,
     })
   )
-  // app.use(
-  //   '/mqtt',
-  //   createProxyMiddleware({
-  //     target: 'http://localhost:5000',
-  //     ws: true,
-  //   })
-  // )
 }
