1e1aa17b57fde42aab173bf1e1f970a2f9c314fa
Pavel Zavora <pavel.zavora@bonitoo.io>
2022-03-04 13:07:47 +0100

    chore(app/ui): upgrade giraffe and react-markdown

--- app/ui/src/util/Markdown.tsx
+++ app/ui/src/util/Markdown.tsx
@@ -1,18 +1,29 @@
 import React, {FunctionComponent, ElementType} from 'react'
 import ReactMarkdown from 'react-markdown'
+import {CodeComponent} from 'react-markdown/lib/ast-to-react'
 import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
 
 interface Props {
   source: string
 }
-const renderers: {[nodeType: string]: ElementType} = {
-  code: ({language, value}) => {
-    return <SyntaxHighlighter language={language} children={value} />
+const renderers: {[nodeType: string]: CodeComponent} = {
+  code: ({inline, children, className}) => {
+    if (inline) {
+      return <code>{children}</code>
+    } else {
+      const language =
+        className && className.startsWith('language-')
+          ? className.substring('language-'.length)
+          : className
+      return (
+        <SyntaxHighlighter language={language}>{children}</SyntaxHighlighter>
+      )
+    }
   },
 }
 
 const Markdown: FunctionComponent<Props> = ({source}) => {
-  return <ReactMarkdown source={source} renderers={renderers} />
+  return <ReactMarkdown components={renderers}>{source}</ReactMarkdown>
 }
 
 export default Markdown
