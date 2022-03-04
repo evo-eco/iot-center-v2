9e1910342170e97afb9029d59dbc59b8f41bfb39
Pavel Zavora <pavel.zavora@bonitoo.io>
2022-03-04 14:00:19 +0100

    chore(app/ui): handle proxy in vite development node

--- app/ui/vite.config.ts
+++ app/ui/vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import envCompatible from 'vite-plugin-env-compatible'
 import svgLoader from '@honkhonk/vite-plugin-svgr'
 
+const SERVER_PORT = process.env.SERVER_PORT || 5000
+
 export default defineConfig({
   plugins: [
     react({fastRefresh: process.env.NODE_ENV !== 'test'}),
@@ -23,5 +25,23 @@ export default defineConfig({
   },
   server: {
     port: process.env.PORT ? Number.parseInt(process.env.PORT) : 3000,
+    proxy: {
+      '/api': {
+        target: `http://localhost:${SERVER_PORT}`,
+        changeOrigin: true,
+      },
+      '/influx': {
+        target: `http://localhost:${SERVER_PORT}`,
+        changeOrigin: true,
+      },
+      '/mqtt': {
+        target: `http://localhost:${SERVER_PORT}`,
+        changeOrigin: true,
+      },
+      '/kafka': {
+        target: `http://localhost:${SERVER_PORT}`,
+        changeOrigin: true,
+      },
+    },
   },
 })
